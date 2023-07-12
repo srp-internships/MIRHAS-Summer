7096adb97f4036b425f60a2526b2d43aa20902be
HasanboyMirdadoev <103140783+HasanboyMirdadoev@users.noreply.github.com>
2023-07-12 16:57:54 +0500

    Finished .net 7 wep api fully

---
+++ Asp.net Web Api/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Generate_Click(object sender, EventArgs e)
+	{
+
+	}
+}
+
